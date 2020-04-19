6b16707612d5d95560f5cb13e06afc022d65fb86
llde <lorenzofer@live.it>
2020-04-19 21:43:10 +0200

    Added CommandInterface

--- TESReloaded/Framework/Plugin.h
+++ TESReloaded/Framework/Plugin.h
@@ -38,3 +38,15 @@ public:
 	const char* Name;
 	UInt32		Version;
 };
+
+
+class CommandTableInterface{
+public:
+	const CommandInfo* (*Start)(void);
+	const CommandInfo* (*End)(void);
+	CommandInfo* (*GetByOpcode)(UInt32 opcode);
+	CommandInfo* (*GetByName)(const char* name);
+	UInt32 (*GetReturnType)(const CommandInfo* cmd);		// return type enum defined in CommandTable.h
+	UInt32 (*GetRequiredOBSEVersion)(const CommandInfo* cmd);
+	const PluginInfo* (*GetParentPlugin)(const CommandInfo* cmd);	// returns a pointer to the PluginInfo of the OBSE plugin that adds the command, if any. returns NULL otherwise
+};
